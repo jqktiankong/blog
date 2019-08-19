00ecd19f5e9eb9b09ae0d28c87f8c07765949aad
姬青柯 <jqk_tiankong@126.com>
2019-08-19 18:00:53 +0800

    日常提交

--- gin_blog/routers/router.go
+++ gin_blog/routers/router.go
@@ -1,7 +1,9 @@
 package routers
 
 import (
+	"blog/gin_blog/middleware/jwt"
 	"blog/gin_blog/pkg/setting"
+	"blog/gin_blog/routers/api"
 	v1 "blog/gin_blog/routers/api/v1"
 	"github.com/gin-gonic/gin"
 )
@@ -15,8 +17,11 @@ func InitRouter() *gin.Engine {
 
 	gin.SetMode(setting.RunMode)
 
+	r.GET("/auth", api.GetAuth)
+
 	// 路由组/api/v1
 	apiv1 := r.Group("/api/v1")
+	apiv1.Use(jwt.JWT())
 	{
 		//获取标签列表
 		apiv1.GET("/tags", v1.GetTags)
@@ -26,6 +31,17 @@ func InitRouter() *gin.Engine {
 		apiv1.PUT("/tags/:id", v1.EditTag)
 		//删除指定标签
 		apiv1.DELETE("/tags/:id", v1.DeleteTag)
+
+		//获取文章列表
+		apiv1.GET("/articles", v1.GetArticles)
+		//获取指定文章
+		apiv1.GET("/articles/:id", v1.GetArticle)
+		//新建文章
+		apiv1.POST("/articles", v1.AddArticle)
+		//更新指定文章
+		apiv1.PUT("/articles/:id", v1.EditArticle)
+		//删除指定文章
+		apiv1.DELETE("/articles/:id", v1.DeleteArticle)
 	}
 
 	return r
